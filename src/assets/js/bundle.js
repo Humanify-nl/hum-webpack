a16ff43864690329f36fba9f54e240b691aee417
Robbert van Hooij <82579622+Humanify-nl@users.noreply.github.com>
2021-09-08 16:41:02 +0200

    add some test html

--- src/assets/js/bundle.js
+++ src/assets/js/bundle.js
@@ -1,2 +1,20 @@
 //import '../css/style.css';
 import Icon from '../icons/icon.png';
+
+function component() {
+  const element = document.createElement('div');
+
+  // Lodash, now imported by this script
+  element.innerHTML = 'Hello webpack';
+  element.classList.add('hello');
+
+ // Add the image to our existing div.
+ const myIcon = new Image();
+ myIcon.src = Icon;
+
+ element.appendChild(myIcon);
+
+  return element;
+}
+
+document.body.appendChild(component());
